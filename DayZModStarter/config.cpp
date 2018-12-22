b00a6201662c471ea3d71ffd6b321c45cda4ad41
Smoke <smokey.edgy@gmail.com>
2018-12-22 18:03:17 -0500

    Fixed spacing part 2

--- DayZModStarter/config.cpp
+++ DayZModStarter/config.cpp
@@ -1,40 +1,40 @@
 class CfgPatches
 {
-	class DayZModStarter
-	{
-		units[]={};
-		weapons[]={};
-		requiredVersion=0.1;
-		requiredAddons[]={};
-	};
+  class DayZModStarter
+  {
+    units[]={};
+    weapons[]={};
+    requiredVersion=0.1;
+    requiredAddons[]={};
+  };
 };
 
 class CfgMods
 {
-	class DayZModStarter
-	{
-		dir = "DayZModStarter";
-		picture = "";
-		action = "";
-		hideName = 1;
-		hidePicture = 1;
-		name = "DayZ Mod Starter";
-		credits = "Smoke";
-		author = "Smoke";
-		authorID = "0";
-		version = "0.1";
-		extra = 0;
-		type = "mod";
+  class DayZModStarter
+  {
+    dir = "DayZModStarter";
+    picture = "";
+    action = "";
+    hideName = 1;
+    hidePicture = 1;
+    name = "DayZ Mod Starter";
+    credits = "Smoke";
+    author = "Smoke";
+    authorID = "0";
+    version = "0.1";
+    extra = 0;
+    type = "mod";
 
-		dependencies[] = { "Mission" };
+    dependencies[] = { "Mission" };
 
-		class defs
-		{
-			class missionScriptModule
-			{
-				value = "";
-				files[] = {"DayZModStarter/5_Mission"};
-			};
-		};
-	};
+    class defs
+    {
+      class missionScriptModule
+      {
+        value = "";
+        files[] = {"DayZModStarter/5_Mission"};
+      };
+    };
+  };
 };
