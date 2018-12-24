c993ab53f38e8b72284e2f311fba4d6b0333bf41
Smoke <smokey.edgy@gmail.com>
2018-12-23 23:53:48 -0500

    Added support for public key file

--- build.bat
+++ build.bat
@@ -1,58 +1,63 @@
-@echo off
+@ECHO OFF
 
 REM Check the provided arguments are what we expect:
 REM modName, version and toolsPath are required
-if [%1]==[] goto usage
-if [%2]==[] goto usage
-if [%3]==[] goto usage
+IF [%1]==[] GOTO usage
+IF [%2]==[] GOTO usage
+IF [%3]==[] GOTO usage
 
 SET modName=%~1
 SET version=%~2
 SET toolsPath=%~3
 SET optionalPrivateKeyFile=%4
 SET optionalPrivateKeyFileArg=
+SET optionalPublicKeyFile=%5
 SET sourceDirectory="%~dp0%modName%"
 SET modDirectory="%~dp0build\@%modName%"
 SET destinationDirectory=%modDirectory%\Addons
 
-@echo.
-@echo ===================================================================
-@echo Building %modName% mod version %version%
-@echo From source directory %sourceDirectory%
-@echo Using DayZ Tools located at %toolsPath%
+@ECHO.
+@ECHO ===================================================================
+@ECHO Building %modName% mod version %version%
+@ECHO From source directory %sourceDirectory%
+@ECHO Using DayZ Tools located at %toolsPath%
 
 REM We will append the -sign argument to AddOnBuilder only if a private key file was provided
-if NOT [%optionalPrivateKeyFile%]==[] (
+IF NOT [%optionalPrivateKeyFile%]==[] (
   SET optionalPrivateKeyFileArg=-sign=%optionalPrivateKeyFile%
-  @echo Signed using private key file %optionalPrivateKeyFile%
+  @ECHO Signed using private key file %optionalPrivateKeyFile%
 )
 
-@echo.
-@echo @%modName% directory and .pbo file will be written to %destinationDirectory%
-@echo ===================================================================
-@echo.
+@ECHO.
+@ECHO @%modName% directory and .pbo file will be written to %destinationDirectory%
+@ECHO ===================================================================
+@ECHO.
 
-if EXIST build rd /S /Q build
+IF EXIST build RD /S /Q build
 
 REM Run the BI Addon build tool, AddonBuilder.exe
 "%toolsPath%\Bin\AddonBuilder\AddonBuilder.exe" %sourceDirectory% %destinationDirectory% -pboversion=%version% -toolsDirectory="%toolsPath%" -clear -prefix=%modName% -include=include.txt %optionalPrivateKeyFileArg%
 
 REM Unfortunately, AddonBuilder always returns an exit code of 0 even on error
 
-md %modDirectory%\Keys
+if NOT [%optionalPublicKeyFile%]==[] (
+  MD %modDirectory%\Keys
+  COPY %optionalPublicKeyFile% %modDirectory%\Keys
+)
 
-exit /B 0
+EXIT /B 0
 
-goto :eof
+GOTO :eof
 :usage
-@echo Usage: build.bat ^<mod name^> ^<version^> ^<DayZ Tools path^> [private key file path]
-@echo.
-@echo A command line tool that wraps around the Bohemia Interactive DayZ Tools AddOnBuilder
-@echo.
-@echo - Generates a client/server ready @modName directory
-@echo - Generates the .pbo file via AddOnBuilder
-@echo - The generated assets above are written to the 'build' directory in the root level of this project
+@ECHO Usage: build.bat ^<mod name^> ^<version^> ^<DayZ Tools path^> [private key file path] [public key file path]
+@ECHO.
+@ECHO A command line tool that wraps around the Bohemia Interactive DayZ Tools AddOnBuilder
+@ECHO.
+@ECHO - Generates a client/server ready @modName directory
+@ECHO - Generates the .pbo file via AddOnBuilder (signed by the private key if provided)
+@ECHO - The public key file (if provided) is copied to the Keys directory under @modName
+@ECHO - The build assets are written to the 'build' directory in the root level of this project
 
-exit /B 1
+EXIT /B 1
 
-@echo on
+@ECHO ON
