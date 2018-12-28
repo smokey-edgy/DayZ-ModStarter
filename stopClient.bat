06da140df0aaa0b171dfd26f264b99d8cfb5cb9f
Smoke <smokey.edgy@gmail.com>
2018-12-28 01:07:57 -0500

    Redirect stderr to null

--- stopClient.bat
+++ stopClient.bat
@@ -3,8 +3,8 @@
 SETLOCAL
 
 REM Ideally, we would wait until these processes have been terminated
-TASKKILL /T /F /IM DayZ_BE.exe
-TASKKILL /T /F /IM DayZ_x64.exe
+TASKKILL /T /F /IM DayZ_BE.exe 2> nul
+TASKKILL /T /F /IM DayZ_x64.exe 2> nul
 
 EXIT /B 0
 
--- stopServer.bat
+++ stopServer.bat
@@ -3,7 +3,7 @@
 SETLOCAL
 
 REM Ideally, we would wait until this process has terminated
-TASKKILL /T /IM DayZServer_x64.exe
+TASKKILL /T /IM DayZServer_x64.exe 2> nul
 
 EXIT /B 0
 
